e5cc2c8bc47bafaf4ab16498dde608c3c9f81003
Brenda Marquez <brenda.jmarq@gmail.com>
2024-07-25 01:45:54 +0000

    second

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -47,9 +47,9 @@ function Navbar() {
   // Componente para las tarjetas de contenido
   function Card(props) {
 	return (
-	  <div className="col-md-3">
+		<div className="col-md-3">
 		<div className="card mb-3 shadow-sm">
-		<img src={props.image} className="card-img-top" alt="..." />
+		<img src={props.image} className="card-img-top" alt="Cat image" />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.text}</p>
